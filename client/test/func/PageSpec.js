cb98d5304111ac4e02cf72cc979c043eb60a617d
Jieyi Hu <fullstackpug@icloud.com>
2017-02-01 18:17:19 -0600

    use webdriverio (selenium client/test runner/test report)

--- client/test/func/PageSpec.js
+++ client/test/func/PageSpec.js
@@ -1,41 +1,10 @@
-const By = require('selenium-webdriver').By;
 const expect = require('chai').expect;
 
-module.exports = (driver) => {
-    describe('Page', () => {
+describe('Page', () => {
 
-        it('renders a <h1/> with proper message', (done) => {
+    it('renders a <h1/> with proper message', () => {
 
-            //  go to localhost
-            driver.get('http://localhost:8080/index.html')
-                .then(() => {
-
-                    //  find header element
-                    driver.findElements(By.className('header'))
-                        .then((elements) => {
-
-                            if ((elements || []).length > 0) {
-                                //  found header element
-                                const header = elements[0];
-
-                                //  get header text
-                                header.getText()
-                                    .then((text) => {
-                                        //  assert
-                                        expect(text).to.equal('Hello World!');
-                                        done();
-                                    });
-                            } else {
-                                //  no header element
-                                throw new Error('Cannot find elements!');
-                            }
-                        });
-                })
-                .catch(done);
-        }).timeout(0);
-
-        afterEach((done) => {
-            driver.quit().then(done).catch(done);
-        });
+        browser.url('http://localhost:8080/index.html');
+        expect(browser.getText('.header=Hello World!')).to.equal('Hello World!');
     });
-};
+});
